963d8ae063ed46f0c3b387e401987ec9e49f5580
Naru-hub <1208ny0324@gmail.com>
2021-08-06 19:52:57 +0900

    add each.rb

--- each.rb
+++ each.rb
@@ -36,3 +36,21 @@ numbers = [1,2,3,4]
 sum = 0
 numbers.each { |n| sum += n }
 puts sum
+
+# map/collect
+numbers = [1,2,3,4,5]
+new_numbers = []
+numbers.each {|n| new_numbers << n * 10 }
+puts new_numbers
+
+# selectメソッド 各要素に対してブロックを評価し、その戻り値が真の要素を集めた配列を返す
+numbers = [1,2,3,4,5,6]
+# ブロックの戻り値が真になった要素だけが集められる(偶数の数値だけを集めた配列を返す)
+even_numbers = numbers.select { |n| n.even?}
+puts even_numbers
+
+# rejectメソッド ブロックの戻り値が真になった要素を除外した配列を返す(ブロックの戻り値が偽である要素を集めるメソッド)
+numbers = [1,2,3,4,5,6]
+# 3の倍数を除外する(3の倍数以外を集める)
+non_multiples_of_three = numbers.reject { |n| n % 3 == 0 }
+puts non_multiples_of_three
