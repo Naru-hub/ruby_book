5a95f54ae65713b64a70f0f7a7af739ba4d85be1
Naru-hub <1208ny0324@gmail.com>
2021-08-15 15:55:09 +0900

    add class.rb

--- class.rb
+++ class.rb
@@ -157,3 +157,127 @@ user = User.new('Alice')
   # 変数に代入しているように見えるが実際はname=メソッドを呼び出している
 user.name = 'Bob'
 puts user.name    #Bob
+
+
+# 上記のコードをattr_accessorで書き換え
+class User
+  # @nameを読み書きするメソッドが自動的に定義される
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+  # nameメソッドやname=メソッドを明示的に定義する必要がない
+end
+user = User.new('Alice')
+# @nameを変更する
+user.name = 'Bob'
+# @nameを参照する
+puts user.name    #Bob
+
+
+# インスタンス変数の内容を読み取り専用にしたい場合はattr_readerメソッドを使う
+class User
+  # 読み取り専用メソッドだけを自動的にする
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+end
+user = User.new('Alice')
+# @nameは省略できる
+puts user.name    #"Alice"
+# @nameを変更しようとするとエラーになる
+
+
+# 書き込み専用にしたい場合attr_writerメソッドを使う
+class User
+  # 書き込み専用のメソッドだけを自動的に定義する
+  attr_writer :name
+
+  def initialize(name)
+    @name = name
+  end
+end
+user = User.new('Alice')
+# @nameは変更できる
+user.name = 'Bob'
+# @nameの参照はできない
+
+
+# カンマで複数の引数を渡すと複数のインスタンス変数に対するアクセサメソッドを作成することができる
+class User
+  # @nameと@ageへのアクセサメソッドを定義する
+  attr_accessor :name,:age
+
+  def initialize(name,age)
+    @name = name
+    @age = age
+  end
+end
+user = User.new('Alice',20)
+puts user.name      #"Alice"
+puts user.age       #20
+
+
+class User
+  def initialize(name)
+    @name = name
+  end
+  # これはインスタンスメソッド
+  def hello
+    # @nameの値はインスタンスによって異なる
+    "Hello,I am #{@name}."
+  end
+end 
+alice = User.new('Alice')
+# インスタンスメソッドはインスタンス(オブジェクト)に対して呼び出す
+puts alice.hello      #"Hello, Iam Alice."
+bob = User.new('Bob')
+# インスタンスのよって内部のデータが異なるのでhelloメソッドの結果も異なる
+puts bob.hello        #"Hello, Iam Bob."
+
+
+# class << selfからendの間にメソッドを書く
+# クラスメソッドを定義する方法その2
+class User
+  def initialize(name)
+    @name = name
+  end
+
+  # self.をつけるとクラスメソッドになる
+  def self.create_users(names)
+    names.map do |name|
+      User.new(name)
+    end
+  end
+  # これはインスタンメソッド
+  def hello
+    "Hello,I am #{@name}."
+  end
+end
+names = ['Alice','Bob','Carol']
+# クラスメソッドの呼び出し
+users = User.create_users(names)
+users.each do |user|
+  # インスタンスメソッドの呼び出し
+  puts user.hello
+end
+# Hello,I am Alice.
+# Hello,I am Bob.
+# Hello,I am Carol
+
+
+# クラスの中には定数を定義できる
+class Product
+  # デフォルトの価格を定数として定義する
+  DEFAULT_PRICE = 0
+  attr_reader :name, :price
+  def initialize(name,price = DEFAULT_PRICE)
+    @name = name
+    @price = price
+  end 
+end
+product = Product.new('A free movie')
+puts product.price   #0
