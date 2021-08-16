158517ef27e365dd9a129aadbf08849ffd3ba5a4
Naru-hub <1208ny0324@gmail.com>
2021-08-16 09:36:51 +0900

    add class.rb

--- class.rb
+++ class.rb
@@ -281,3 +281,92 @@ class Product
 end
 product = Product.new('A free movie')
 puts product.price   #0
+
+
+# selfなしでnameメソッドを呼び出す場合、self付きで呼び出す場合、直接インスタンス変数にアクセスする場合
+class User
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def hello
+    # selfなしでnameメソッドを呼ぶ
+    "Hello,I am #{name}."
+  end
+
+  def hi
+    #self付きでnameメソッドを呼ぶ
+    "Hi, I am #{self.name}."
+  end
+
+  def my_name
+    # 直接インスタンス変数の@nameにアクセスする
+    "My name is #{@name}."
+  end
+end
+user = User.new('Alice')
+puts user.hello        #Hello,I am Alice.
+puts user.hi           #Hi, I am Alice.
+puts user.my_name      #My name is Alice.
+
+
+# selfつけ忘れで不具合が発生するケース
+class User
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def rename_to_bob
+    #selfなしでname=メソッドを呼ぶ(?) nemeというローカル変数に"Bob"という文字列に代入したと解釈される 下のようにself.をつける
+    name = 'Bob'
+  end
+
+  def rename_to_carol
+    #self付きでname=メソッドを呼ぶ
+    self.name = 'Carol'
+  end
+
+  def rename_to_dave
+    # 直接インスタンス変数を書き換える
+    @name = 'Dave'
+  end
+end
+user = User.new('Alice')
+# Bobにリネーム...できていない!!
+user.rename_to_bob
+puts user.name     #"Alice"
+
+# Carolのリネーム
+puts user.rename_to_carol
+user.name          #"Carol"
+
+# Daveにリネーム
+user.rename_to_dave
+puts user.name     #"Dave"
+
+
+class Product
+  attr_reader :name, :price
+
+  def initialize(name,price)
+    @name = name
+    @price = price
+  end
+
+  # 金額を整形するクラスメソッド
+  def self.format_price(price)
+    "#{price}円"
+  end
+
+  def to_s
+    # インスタンスメソッドからクラスメソッドを呼び出す
+    formatted_price = Product.format_price(price)
+    "name: #{name}, price: #{formatted_price}"
+  end
+end
+product = Product.new('A grat movie', 1000)
+puts product.to_s     #"name: A grat movie, price: 1000円"
