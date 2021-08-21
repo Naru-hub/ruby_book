1e9d36ad110f21797cf51512ced684d0aa33afbb
Naru-hub <1208ny0324@gmail.com>
2021-08-21 14:18:06 +0900

    add module.rb include

--- deep_freeze.rb
+++
@@ -1,8 +0,0 @@
-class Team
-  # 配列をfreezeして要素の追加や削除を禁止する
-  COUNTRIES = deep_freeze(['Japan','US','India'])
-end
-
-# 配列自身と配列の全要素がfreezeされている
-puts Team::COUNTRIES.frozen?
-
--- module.rb
+++ module.rb
@@ -66,3 +66,96 @@ puts product.title           #A great movie
 
 puts user = User.new         #[LOG] name is called.
 puts user.name               #Alice
+
+
+# モジュールをextendする
+# モジュールの定義はincludeするときと同じ
+module Loggable
+  def log(text)
+    puts "[LOG] #{text}"
+  end
+end
+
+class Product
+  # Loggableモジュールのメソッドを特異メソッド(クラスメソッド)としてミックスインする
+  extend Loggable
+
+  def self.create_products(names)
+    # logメソッドをクラスメソッド内で呼び出す
+    #(つまりlogメソッド自体もクラスメソッドになっている)
+    log 'create_products is called.'
+    # 他の実装は省略
+  end
+end
+# クラスメソッド経由でlogメソッドが呼び出される
+puts Product.create_products([])     #[LOG] create_products is called.
+# Productクラスのクラスメソッドとして直接呼び出すことも可能
+puts Product.log('Hello.')           #[LOG] Hello.
+
+class Product
+  extend Loggable
+
+  # logメソッドをクラスの構文の直下で呼び出す
+  # (クラスが読みこまれるタイミングでこのlogメソッドも実行される)
+  puts log 'Defined Product class.'
+end
+#[LOG] Defined Product class.
+
+
+# includeされたモジュールの有無
+module Loggable
+  # 省略
+end
+class Product
+  include Loggable
+  # 省略
+  end
+  puts Product.include?(Loggable)    #true
+  puts Product.included_modules      #[Loggable, Kernel]
+  puts Product.ancestors             #[Product,Loggable,Object,Kernel,BasicObject]
+
+product = Product.new
+# product.classはProductクラスを返す
+puts product.class.include?(Loggable)   #true
+puts product.class.included_modules     #[Loggable,Kernel]
+
+product = Product.new
+# 引数が自クラス,includeしているモジュール、スーパークラスのいずれかに該当すればtrue
+puts product.is_a?(Product)   #true
+puts product.is_a?(Loggable)  #true
+puts product.is_a?(Object)    #true
+
+
+# include先のメソッドを使うモジュール
+module Taggable
+  def price_tag
+    # priceメソッドはinclude先で定義されているはず、という前提
+    "#{price}円"
+  end
+end
+
+class Product
+include Taggable
+  def price
+    1000
+  end
+end
+
+puts product = Product.new
+puts product.price_tag    #1000円
+
+module Taggable
+  def price_tag
+    # あえてselfをつけて呼び出してもいい(privateメソッドでなければ)
+    # selfはinclude先のクラス(たとえばProductクラス)のインスタンスになる
+    "#{self.price}円"
+  end
+end
+
+
+# Enumerableモジュール(配列やハッシュ、範囲(Range)など何かしらの繰り返し処理ができるクラスにincludeされているモジュールです
+puts Array.include?(Enumerable)    #true
+puts Hash.include?(Enumerable)     #true
+puts Range.include?(Enumerable)    #true
+puts [1,2,3].map {|n| n * 10}      #[10,20,30]
+puts (1..3).map {|n| n * 10}       #[10,20,30]
